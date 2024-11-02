bb304da0296fe04825f7676f9fd3b2e25878db2f
Ujjwal Sharma <fallparker2@gmail.com>
2024-11-02 21:58:09 +0530

    Fixed db code

--- tracker-server/db/db.js
+++ tracker-server/db/db.js
@@ -1,7 +1,9 @@
 import { mongoose,Schema} from 'mongoose';
 const MONGO_URL=process.env.MONGO_URL || "mongodb://localhost:27017/Users";
-mongoose.connect(MONGO_URL).catch(error=>console.log(error));
-console.log("Db Connected Succesfully");
+mongoose.connect(MONGO_URL).then(()=>{
+    console.log("Db Connected Succesfully");
+}).catch(error=>console.log(error));
+
 const userSchema=new Schema({
     username: {
         type: String,
